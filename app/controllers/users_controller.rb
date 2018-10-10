437aba9df9c976789ff6cf48c6051401d3e752bb
dmeskis <dmeskis@gmail.com>
2018-10-10 14:39:41 -0600

    Visitor can see multiple users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
     service = UserService.new(params)
     @user = User.new(service.find_user)
   end
+
+  def index
+    service = UserService.new(params)
+    users = service.all_users
+    @users = users.map {|user| User.new(user)}
+  end
 end
--- spec/features/visitor_can_see_users_spec.rb
+++ spec/features/visitor_can_see_users_spec.rb
@@ -3,14 +3,28 @@ require 'rails_helper'
 describe 'as a visitor' do
   describe 'visit /api/v1/users/:id' do
     it 'shows a users name and email address' do
-
-      # stub_request(:any, "http://localhost:3000").
-      #   to_return(body: "abc", status: 200)
+      file = File.open("./fixtures/single_user.json")
+      stub_request(:any, "http://localhost:3000").
+        to_return(body: file, status: 200)
 
       visit "/users/1"
 
       expect(page).to have_content("Josiah Bartlet")
       expect(page).to have_content("jbartlet@example.com")
+
+    end
+    it 'shows multiple users name and email address' do
+
+      file = File.open("./fixtures/multiple_users.json")
+      stub_request(:get, "http://localhost:3000/api/v1/users").
+        to_return(body: file, status: 200)
+
+      visit "/users"
+
+      expect(page).to have_content("Josiah Bartlet")
+      expect(page).to have_content("jbarlet@example.com")
+      expect(page).to have_content("Dylan Meskis")
+      expect(page).to have_content("dmeskis@gmail.com")
 
     end
   end
