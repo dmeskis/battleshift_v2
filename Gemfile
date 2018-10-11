ebf32233eab30ee87c06fd9e572575b71a1f6829
dmeskis <dmeskis@gmail.com>
2018-10-11 15:18:49 -0600

    Adds faker gem and changes user factory to use faker

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ group :development, :test do
   gem 'capybara'
   gem 'database_cleaner'
   gem 'launchy'
+  gem 'faker'
 end
 
 group :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates_presence_of :password, require: true
+  validates_presence_of :email
 
   has_secure_password
 end
