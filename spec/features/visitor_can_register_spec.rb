61237d6188564be6cecad8bee53df6e649bc9732
dmeskis <dmeskis@gmail.com>
2018-10-11 14:44:34 -0600

    Wrote test for user registering

---
+++ spec/features/visitor_can_register_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+describe 'on the landing page' do
+  describe 'as a visitor' do
+    it 'allows me to register and be logged into a dashboard' do
+      username = "Bob"
+      email = "Bob@mail.com"
+      password = "password"
+
+      visit '/'
+      click_on "Register"
+      fill_in :email_address, with: email
+      fill_in :username, with: username
+      fill_in :password, with: password
+      fill_in :password_confirmation, with: password
+      click_on "Submit"
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Logged in as #{username}")
+      expect(page).to have_content("This account has not yet been activated. Please check your email.")
+    end
+    it 'can fail to register if passwords do not match' do
+      username = "Bob"
+      email = "Bob@mail.com"
+      password = "password"
+
+      visit '/'
+      click_on "Register"
+      fill_in :email_address, with: email
+      fill_in :username, with: username
+      fill_in :password, with: password
+      fill_in :password_confirmation, with: "123"
+      click_on "Submit"
+
+      expect(current_path).to eq(new_user_path)
+      expect(page).to have_content("Failed to register. Please try again.")
+    end
+  end
+end
+
+# As a guest user
+# When I visit "/"
+# And I click "Register"
+# Then I should be on "/register"
+# And when I fill in an email address (required)
+# And I fill in name (required)
+# And I fill in password and password confirmation (required)
+# And I click submit
+# Then I should be redirected to "/dashboard"
+# And I should see a message that says "Logged in as <SOME_NAME>"
+# And I should see "This account has not yet been activated. Please check your email."
