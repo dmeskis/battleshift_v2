cc84ae9f9cf8c829758934837040e03278951b04
dmeskis <dmeskis@gmail.com>
2018-10-12 16:22:25 -0600

    Working on account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,22 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+
   validates :name, uniqueness: true, presence: true
   validates_presence_of :password, on: :create
   validates_presence_of :email
   enum status: %w(unactivated activated)
 
   has_secure_password
+
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/dashboard', to: 'dashboard#index'
   resources :users, only: [:show, :index, :edit, :update, :create, :new]
+  resources :account_activations, only: [:edit]
 end
