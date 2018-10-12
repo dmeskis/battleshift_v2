e1e276aadfbc94b123e61426affa5b3f7185ffdb
dmeskis <dmeskis@gmail.com>
2018-10-12 14:00:16 -0600

    in process of writing test for activating a user account

--- spec/features/non-activated_user_can_activate_account_spec.rb
+++ spec/features/non-activated_user_can_activate_account_spec.rb
@@ -3,8 +3,17 @@ require 'rails_helper'
 describe 'non-activated user' do
   describe 'clicks validation link in email' do
     it 'takes user to a page confirming your account is activated' do
-      user = create(:user)
+      username = "Bob"
+      email = "Bob@mail.com"
+      password = "password"
 
+      visit '/'
+      click_on "Register"
+      fill_in :user_email, with: email
+      fill_in :user_name, with: username
+      fill_in :user_password, with: password
+      fill_in :user_password_confirmation, with: password
+      click_on "Submit"
     end
   end
 end
