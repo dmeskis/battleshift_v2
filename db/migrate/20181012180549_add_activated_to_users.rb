f24c4c58f30180e932279b799264d67c3b5306bb
dmeskis <dmeskis@gmail.com>
2018-10-12 12:56:19 -0600

    Adds status to user

--- db/migrate/20181012180549_add_activated_to_users.rb
+++ db/migrate/20181012180549_add_activated_to_users.rb
@@ -1,5 +1,5 @@
 class AddActivatedToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :activated, :integer, default: => 0
+    add_column :users, :status, :integer, default: 0
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe User, type: :model do
   it {should validate_presence_of(:name)}
   it {should validate_presence_of(:email)}
   it {should validate_presence_of(:password)}
+
+  describe 'status' do
+    it 'is is unactivated upon creation' do
+      user = create(:user)
+
+      expect(user.status).to eq("unactivated")
+      expect(user.activated?).to be_falsey
+    end
+
+    it 'can be activated' do
+      user = create(:user, status: 1)
+
+      expect(user.status).to eq("activated")
+      expect(user.activated?).to be_truthy
+    end
+  end
 end
