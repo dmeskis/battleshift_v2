9bf4b87fd1617fda4b43c97f564c6357e28c1652
dmeskis <dmeskis@gmail.com>
2018-10-12 16:26:18 -0600

    added downcase email to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
-  before_create :create_activation_digest
 
   validates :name, uniqueness: true, presence: true
   validates_presence_of :password, on: :create
@@ -13,10 +11,5 @@ class User < ApplicationRecord
   private
     def downcase_email
       self.email = email.downcase
-    end
-
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
     end
 end
