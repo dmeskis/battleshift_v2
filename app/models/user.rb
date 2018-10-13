54319f3b3d842de4689866eef183aeb9c87ca2e9
dmeskis <dmeskis@gmail.com>
2018-10-12 18:30:32 -0600

    Adds activation_digest to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
+  before_create :create_activation_digest
 
   validates :name, uniqueness: true, presence: true
   validates_presence_of :password, on: :create
@@ -8,8 +10,24 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   private
+
     def downcase_email
       self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 end
--- db/migrate/20181012180549_add_activated_to_users.rb
+++ db/migrate/20181012180549_add_activated_to_users.rb
@@ -1,5 +1,6 @@
 class AddActivatedToUsers < ActiveRecord::Migration[5.1]
   def change
+    add_column :activation_digest, :string
     add_column :users, :status, :integer, default: 0
   end
 end
