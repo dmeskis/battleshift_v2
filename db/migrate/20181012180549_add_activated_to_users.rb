0b9fd08aa2a241eea1745a00d0ea494ce3f2f97c
dmeskis <dmeskis@gmail.com>
2018-10-12 20:31:30 -0600

    Adds remember to user

--- db/migrate/20181012180549_add_activated_to_users.rb
+++ db/migrate/20181012180549_add_activated_to_users.rb
@@ -1,5 +1,6 @@
 class AddActivatedToUsers < ActiveRecord::Migration[5.1]
   def change
+    add_column :users, :remember_digest, :string
     add_column :users, :activation_digest, :string
     add_column :users, :activated, :integer, default: 0
     add_column :users, :activated_at, :datetime
