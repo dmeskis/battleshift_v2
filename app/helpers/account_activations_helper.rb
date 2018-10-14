ecf6522f0b9f8b8ac0422df3d20b18c4191f551e
dmeskis <dmeskis@gmail.com>
2018-10-14 12:38:14 -0600

    Deleted code in account activations helper

--- app/helpers/account_activations_helper.rb
+++ app/helpers/account_activations_helper.rb
@@ -1,7 +1,2 @@
 module AccountActivationsHelper
-  def activation(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
 end
