10bfb29dfb43c29d71616d49fa63f962dee26b9e
dmeskis <dmeskis@gmail.com>
2018-10-14 13:18:38 -0600

    Refactored UserLogic into BattleshiftApi::User

--- spec/models/battleshift_api/user_spec.rb
+++ spec/models/battleshift_api/user_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
-describe UserLogic, type: :model do
+describe BattleshiftApi::User, type: :model do
   it 'exists' do
-    user_logic = UserLogic.new()
+    user = BattleshiftApi::User.new()
 
-    expect(user_logic).to be_a(UserLogic)
+    expect(user).to be_a(BattleshiftApi::User)
   end
 
   context 'instance methods' do
     context 'single_user' do
       it 'returns a single user' do
-        logic = UserLogic.new(id: 1)
-        expect(logic.single_user).to be_a(User)
+        user = BattleshiftApi::User.new(id: 1)
+        expect(user.single_user).to be_a(User)
       end
     end
     context 'many_user' do
       it 'returns many users' do
-        logic = UserLogic.new()
-        users = logic.many_users
+        user = BattleshiftApi::User.new()
+        users = user.many_users
         expect(users.count).to be > 1
         expect(users).to all be_a User
       end
