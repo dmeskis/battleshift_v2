12898c32aac5b03a7c6db14706cca3fe6b48e85e
dmeskis <dmeskis@gmail.com>
2018-10-14 12:35:59 -0600

    Mailer works in development

--- app/helpers/account_activations_helper.rb
+++ app/helpers/account_activations_helper.rb
@@ -1,7 +1,7 @@
 module AccountActivationsHelper
   def activation(user)
-  user.remember
-  cookies.permanent.signed[:user_id] = user.id
-  cookies.permanent[:remember_token] = user.remember_token
-end
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 end
