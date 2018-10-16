cfa7dc4dcca54d43ee15cbdfa87f53fd6a864156
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-10-15 19:03:54 -0600

    Adds some refactoring

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -2,28 +2,10 @@ module Api
   module V1
     module Games
       class ShipsController < ApiController
+        
         def create
-          game = Game.find(params[:game_id])
-          api_key = request.headers['X-Api-Key']
-          if api_key == game.challenger.api_key
-            board = game.player_1_board
-          elsif api_key == game.opponent.api_key
-            board = game.player_2_board
-          end
-          ship_placer = ShipPlacer.new(board: board,
-                                       ship: Ship.new(params[:ship_size]),
-                                       start_space: params[:start_space],
-                                       end_space: params[:end_space]
-                                      )
-
-          if params[:ship_size] == 3
-            remaining_ships = 1
-            size = 2
-          elsif params[:ship_size] == 2
-            remaining_ships = 0
-          end
-
-
+          player_board
+          ship_sizes
           if ship_placer.run && params[:ship_size] == 3
             game = Game.find(params[:game_id])
             render json: game, message: "Successfully placed ship with a size of #{params[:ship_size]}. You have #{remaining_ships} ship(s) to place with a size of #{size}."
@@ -34,6 +16,36 @@ module Api
             render status: 400
           end
         end
+        
+        private 
+          def ship_placer
+            ShipPlacer.new( board: board,
+                            ship: Ship.new(params[:ship_size]),
+                            start_space: params[:start_space],
+                            end_space: params[:end_space]
+                          )
+          end 
+          
+          def api_key
+            request.headers['X-Api-Key']
+          end
+          
+          def player_board 
+            if api_key == game.challenger.api_key
+              board = game.player_1_board
+            elsif api_key == game.opponent.api_key
+              board = game.player_2_board
+            end
+          end
+          
+          def ship_sizes
+            if params[:ship_size] == 3
+              remaining_ships = 1
+              size = 2
+            elsif params[:ship_size] == 2
+              remaining_ships = 0
+            end
+          end
       end
     end
   end
--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class GamesController < ActionController::API
+      
       def show
         game = Game.find_by(id: params[:id])
         if game
@@ -11,19 +12,6 @@ module Api
       end
 
       def create
-        challenger = User.find_by(api_key: request.headers['X-Api-Key'])
-        opponent = User.find_by(email: params[:opponent_email])
-        player_1_board = Board.new(4)
-        player_2_board = Board.new(4)
-        game_attributes = {
-                        player_1_board: player_1_board,
-                        player_2_board: player_2_board,
-                        player_1_turns: 0,
-                        player_2_turns: 0,
-                        current_turn: "challenger",
-                        challenger: challenger,
-                        opponent: opponent
-                      }
         game = Game.new(game_attributes)
         if game.save
           render json: game, status: 200
@@ -31,6 +19,27 @@ module Api
           render status: 400
         end
       end
+      
+      private 
+        def game_attributes
+          {
+            player_1_board: Board.new(4),
+            player_2_board: Board.new(4),
+            player_1_turns: 0,
+            player_2_turns: 0,
+            current_turn: "challenger",
+            challenger: challenger,
+            opponent: opponent
+          }
+        end 
+        
+        def challenger
+          User.find_by(api_key: request.headers['X-Api-Key'])
+        end 
+        
+        def opponent
+          User.find_by(email: params[:opponent_email])   
+        end
     end
   end
 end
