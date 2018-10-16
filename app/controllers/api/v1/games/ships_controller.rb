40aa8d283f58aa7bd73f106d4e347163a59338f4
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-10-15 19:51:59 -0600

    changes first ships_controller refactoring

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -2,10 +2,18 @@ module Api
   module V1
     module Games
       class ShipsController < ApiController
-        
+    
         def create
-          player_board
-          ship_sizes
+          @game = Game.find(params[:game_id])
+          players_board
+                                  
+          if params[:ship_size] == 3
+            remaining_ships = 1
+            size = 2
+            elsif params[:ship_size] == 2
+            remaining_ships = 0
+          end
+
           if ship_placer.run && params[:ship_size] == 3
             game = Game.find(params[:game_id])
             render json: game, message: "Successfully placed ship with a size of #{params[:ship_size]}. You have #{remaining_ships} ship(s) to place with a size of #{size}."
@@ -17,33 +25,24 @@ module Api
           end
         end
         
-        private 
+        private
+          def api_key
+            request.headers['X-Api-Key']
+          end 
+          
           def ship_placer
-            ShipPlacer.new( board: board,
+            ShipPlacer.new( board: @board,
                             ship: Ship.new(params[:ship_size]),
                             start_space: params[:start_space],
                             end_space: params[:end_space]
                           )
-          end 
-          
-          def api_key
-            request.headers['X-Api-Key']
           end
           
-          def player_board 
-            if api_key == game.challenger.api_key
-              board = game.player_1_board
-            elsif api_key == game.opponent.api_key
-              board = game.player_2_board
-            end
-          end
-          
-          def ship_sizes
-            if params[:ship_size] == 3
-              remaining_ships = 1
-              size = 2
-            elsif params[:ship_size] == 2
-              remaining_ships = 0
+          def players_board 
+            if api_key == @game.challenger.api_key
+              @board = @game.player_1_board
+            elsif api_key == @game.opponent.api_key
+              @board = @game.player_2_board
             end
           end
       end
